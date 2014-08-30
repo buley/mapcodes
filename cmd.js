cb48068e513204fa760a0fc3ed4fa61e79dd73f6
Taylor Buley <taylor.buley@gmail.com>
2014-08-29 21:47:44 -0700

    WIP

--- cmd.js
+++ cmd.js
@@ -1,4 +1,4 @@
-var mapcodes = require('./src/mapcodes.js');
+var mapcodes = require( __dirname + '/src/mapcodes.js');
 if ( 4 === process.argv.length ) {
 	console.log( mapcodes.decode( process.argv[3], process.argv[2] ) );
 } else if ( 5 === process.argv.length ) {
